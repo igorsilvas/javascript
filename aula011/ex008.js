2d78916f1821c964dc6041f68c720175ff923bb1
Ygor Silva <113484193+igorsilvas@users.noreply.github.com>
2022-09-18 04:22:05 -0300

    JavaScript

--- aula011/ex008.js
+++ aula011/ex008.js
@@ -1,1 +1,6 @@
-console.log('O console funcionou perfeitamente')
+var vel = 50
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {//condição simples!
+    console.log('Voçe ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ aula011/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log('Voçe é Brasileiro!')
+} else {
+    console.log('Voçe é Estrangeiro!')
+}
